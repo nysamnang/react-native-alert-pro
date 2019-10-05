684ac354fc23b4360c7bc30549f9c3a3a76e6e3b
Ny Samnang <nysamnang168@gmail.com>
2019-10-05 17:17:43 +0700

    using typescript

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+import { Component } from "react";
+import { StyleProp, ViewStyle, TextStyle } from "react-native";
+
+declare module "react-native-alert-pro" {
+  export type AlertProProps = {
+    title?: string;
+    message?: string;
+    showCancel?: boolean;
+    showConfirm?: boolean;
+    textCancel?: string;
+    textConfirm?: string;
+    closeOnPressMask?: boolean;
+    customStyles?: {
+      title?: StyleProp<TextStyle>;
+      message?: StyleProp<TextStyle>;
+      mask?: StyleProp<ViewStyle>;
+      container?: StyleProp<ViewStyle>;
+      buttonCancel?: StyleProp<ViewStyle>;
+      buttonConfirm?: StyleProp<ViewStyle>;
+      textCancel?: StyleProp<TextStyle>;
+      textConfirm?: StyleProp<TextStyle>;
+    };
+    onCancel?: () => void;
+    onConfirm?: () => void;
+    onClose?: () => void;
+  };
+
+  class AlertPro extends Component<AlertProProps> {
+    open(): void;
+    close(): void;
+  }
+
+  export default AlertPro;
+}
